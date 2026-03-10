d6cb72232b84d992bcad9f5c964e4ec712e94ca9
SushiDotRoll <irsyad.spramudito@gmail.com>
2026-03-11 05:20:44 +0700

    menambahkan implementasi dengan table driven

--- tpmodul4_103082400030/Program.cs
+++ tpmodul4_103082400030/Program.cs
@@ -1,1 +1,35 @@
-﻿Console.WriteLine("Hello, World!");
+﻿using System;
+
+class Program
+{
+    static void TampilkanKelurahan()
+    {
+        string[,] data =
+        {
+            {"Batununggal","40266"},
+            {"Kujangsari","40287"},
+            {"Mengger","40267"},
+            {"Wates","40256"},
+            {"Cijaura","40287"},
+            {"Jatisari","40286"},
+            {"Margasari","40286"},
+            {"Sekejati","40286"},
+            {"Kebonwaru","40272"},
+            {"Maleer","40274"},
+            {"Samoja","40273"}
+        };
+
+        Console.WriteLine("{0,-15} {1}", "Kelurahan", "Kode Pos");
+
+        for (int i = 0; i < data.GetLength(0); i++)
+        {
+            Console.WriteLine("{0,-15} {1}", data[i, 0], data[i, 1]);
+        }
+    }
+
+    static void Main(string[] args)
+    {
+        TampilkanKelurahan();
+        Console.ReadLine();
+    }
+}
--- tpmodul4_103082400030/obj/Debug/net10.0/tpmodul4_103082400030.AssemblyInfo.cs
+++ tpmodul4_103082400030/obj/Debug/net10.0/tpmodul4_103082400030.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("tpmodul4_103082400030")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+72e3472f4716c6fd9e214ea314a395ae214c3f74")]
 [assembly: System.Reflection.AssemblyProductAttribute("tpmodul4_103082400030")]
 [assembly: System.Reflection.AssemblyTitleAttribute("tpmodul4_103082400030")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
